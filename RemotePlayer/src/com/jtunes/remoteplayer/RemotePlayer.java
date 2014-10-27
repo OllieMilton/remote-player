8b03da822219305f53a8074b84d8ae1e52b25d2f
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2014-10-27 21:47:44 +0000

    Now connects to jaudio stream server

--- RemotePlayer/src/com/jtunes/remoteplayer/RemotePlayer.java
+++ RemotePlayer/src/com/jtunes/remoteplayer/RemotePlayer.java
@@ -1,6 +1,9 @@
 package com.jtunes.remoteplayer;
 
+import java.net.ConnectException;
+
 import oaxws.annotation.WebService;
+import oaxws.annotation.WsMethod;
 import serialiser.factory.SerialiserFactory;
 
 import com.jaudiostream.client.JAudioStreamClient;
@@ -21,13 +24,18 @@ public class RemotePlayer extends RemoteClient implements AudioPlayerEventListen
 	
 	private JAudioStreamClient jaudioStream;
 	private AudioPlayer player;
+	private String name;
+	private boolean playing;
+	private boolean paused;
 		
 	public RemotePlayer() {
 		super(SerialiserFactory.getJsonSerialiser(new JTunesTypeRegistry()));
+		name = "Remote player";
 		jaudioStream = new JAudioStreamClient();
 		player = new AudioPlayer(this, jaudioStream.getInputStream());
 	}
 	
+	@Override
 	protected void start(String user, String password) {
 		super.start(user, password);
 		startComplete();
@@ -35,7 +43,21 @@ public class RemotePlayer extends RemoteClient implements AudioPlayerEventListen
 	
 	@Override
 	protected void loggedIn() {
-		client.registerRemoteDevice("", DeviceType.REMOTE_PLAYER);
+		client.registerRemoteDevice(name, DeviceType.REMOTE_PLAYER);
+		String audioStreamAddress = getServerAudioAddress();
+		if (audioStreamAddress != null) {
+			String[] addr = audioStreamAddress.split(",");
+			try {
+				jaudioStream.connect(addr[0], Integer.valueOf(addr[1]).intValue(), name);
+			} catch (ConnectException | NumberFormatException e) {
+				logger.error("Could not connect to audio streaming service.");
+				fatalError();
+			}
+		} else {
+			logger.error("Jtunes did mot respond with audio streaming service address.");
+			fatalError();
+		}
+		client.registerStreamingClient(jaudioStream.getLocalPort());
 	}
 		
 	@Override
@@ -47,20 +69,53 @@ public class RemotePlayer extends RemoteClient implements AudioPlayerEventListen
 			jaudioStream.getInputStream().close();
 		} catch (Exception e) {}
 	}
+	
+	@WsMethod(name=RemotePlayerService.pause)
+	void pause() {
+		player.pause();
+	}
+	
+	@WsMethod(name=RemotePlayerService.stop)
+	void stop() {
+		player.stop();
+	}
+	
+	@WsMethod(name=RemotePlayerService.play)
+	void play() {
+		if (playing && !paused) {
+            pause();    
+        } else {
+            if (!playing) {
+                logger.info("Play received, waiting for clear stream. Playing ["+playing+"], paused ["+paused+"], clear stream ["+jaudioStream.isClear()+"]");
+                while (!playing && !jaudioStream.isClear() && !paused) {
+                    try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+						
+					}
+                }    
+                logger.info("Got clear stream.");
+                player.play();    
+            }        
+        }
+	}
 
 	@Override
 	public void onStopped() {
-			
+		playing = false;
+    	paused = false;	
 	}
 
 	@Override
 	public void onPlaying() {
-			
+		playing = true;  
+        paused = false;  
 	}
 
 	@Override
 	public void onPaused() {
-			
+		playing = false;    
+        paused = true;	
 	}
 
 }
